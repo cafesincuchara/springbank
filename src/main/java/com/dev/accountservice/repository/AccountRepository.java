37be8aa5a55cc5e6d0670317ad01c4f1c8f0cdc3
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-03-25 18:10:47 -0300

    feat: nuevas funciones en AccountController para poder hacer las peticiones de crear cuentas y obtener cuentas

--- src/main/java/com/dev/accountservice/repository/AccountRepository.java
+++ src/main/java/com/dev/accountservice/repository/AccountRepository.java
@@ -1,11 +1,13 @@
 package com.dev.accountservice.repository;
 
 import com.dev.accountservice.domain.Account;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
+@EnableR2dbcRepositories
 public interface AccountRepository extends ReactiveCrudRepository<Account, Long> {
 
     Mono<Account> findByAccountNumber(String accountNumber);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS accounts (
     id SERIAL PRIMARY KEY,
     account_number VARCHAR(20) UNIQUE NOT NULL,
     customer_id VARCHAR(50) NOT NULL,
-    balance DECIMAL(15,2) DEFAULT 0.00,
-    status VARCHAR(20) DEFAULT 'ACTIVE'
+    balance DECIMAL(15, 2) DEFAULT 0.00,
+    status VARCHAR(20) NOT NULL DEFAULT 'ACTIVE'
     );
