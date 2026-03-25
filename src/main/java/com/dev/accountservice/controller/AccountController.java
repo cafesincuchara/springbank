b0d9e76da89dca856bbf895f1d22bc84b8fdcf15
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-03-25 18:10:47 -0300

    feat: nuevas funciones en AccountController para poder hacer las peticiones de crear cuentas y obtener cuentas

--- src/main/java/com/dev/accountservice/controller/AccountController.java
+++ src/main/java/com/dev/accountservice/controller/AccountController.java
@@ -1,4 +1,29 @@
 package com.dev.accountservice.controller;
 
+import com.dev.accountservice.dto.AccountRequestDTO;
+import com.dev.accountservice.dto.AccountResponseDTO;
+import com.dev.accountservice.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/accounts")
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<AccountResponseDTO> create(@RequestBody AccountRequestDTO request){
+        return service.createAccount(request);
+    }
+
+    @GetMapping
+    public Flux<AccountResponseDTO> getAll(){
+        return service.findAll();
+    }
 }
